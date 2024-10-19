b5164c1ab087efa83a2d6ca465f53511e5a7cecf
Kent Ng <kent.ng201@gmail.com>
2024-10-19 21:04:27 +0800

    change id method to timestamp based

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 import { DatabaseManager, p } from 'pocketto'
 
 p.setEnvironment('browser');
+p.setIdMethod('timestamp');
 DatabaseManager.connect('default', {
     dbName: 'default',
 }).then(async (localDb) => {
